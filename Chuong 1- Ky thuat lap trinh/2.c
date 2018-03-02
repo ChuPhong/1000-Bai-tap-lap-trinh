f54ee3ae638d5423fe8fc9067c248235a7cd3ec9
ChuPhong <36165008+ChuPhong@users.noreply.github.com>
2018-03-02 12:12:30 +0700

    Bài giải Chương 1 - Bài 2

---
+++ Chuong 1- Ky thuat lap trinh/2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Tính S(n) = 1^2 + 2^2 + 3^2 + … + n^2
+
+int main() {
+    int n = 5;
+    int sum = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        sum += i*i;
+    }
+
+    printf("%d", sum);
+
+	return 0;
+}
