2ca35c28602f2b563b7c4ff9e6f218ed3a85f12d
ChuPhong <36165008+ChuPhong@users.noreply.github.com>
2018-03-18 19:46:42 +0700

    Update 1.c

--- Chuong 1- Ky thuat lap trinh/1.c
+++ Chuong 1- Ky thuat lap trinh/1.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 
-// Tính S(n) = 1 + 2 + 3 + ... + n.
+int DeQuy(int n);
+int KhuDeQuy(int number);
 
-int main() {
-    int n = 10;
+int DeQuy(int n)
+{
+    if (n <= 1) return 1;
+
+    return DeQuy(n - 1) + n;
+}
+
+int KhuDeQuy(int number)
+{
     int sum = 0;
-
-    for (int i = 1; i <= 10; ++i) {
+    for (int i = 1; i <= number; ++i)
+    {
         sum += i;
     }
+    return sum;
+}
 
-    printf("%d", sum);
+int main()
+{
+    int number;
+
+    printf("Number: ");
+    scanf("%d", &number);
+
+    printf("De quy: %d\n", DeQuy(number));
+    printf("Khu de quy: %d", KhuDeQuy(number));
 
     return 0;
 }
