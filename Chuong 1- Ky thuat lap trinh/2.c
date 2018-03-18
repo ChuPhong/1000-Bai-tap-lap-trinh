4451172ff5a8387067c93e1053383632d0e6b4d5
ChuPhong <36165008+ChuPhong@users.noreply.github.com>
2018-03-18 20:05:24 +0700

    Update 2.c

--- Chuong 1- Ky thuat lap trinh/2.c
+++ Chuong 1- Ky thuat lap trinh/2.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
+#include <math.h>
 
-// Tính S(n) = 1^2 + 2^2 + 3^2 + ... + n^2
+int DeQuy(int n);
+int KhuDeQuy(int n);
 
-int main() {
-    int n = 5;
+int DeQuy(int n)
+{
+    if (n <= 1)
+        return 1;
+    return DeQuy(n - 1) + pow(n, 2);
+}
+
+int KhuDeQuy(int n)
+{
     int sum = 0;
 
-    for (int i = 1; i <= n; ++i) {
-        sum += i*i;
+    for (int i = 1; i <= n; ++i)
+    {
+        sum += pow(i, 2);
     }
 
-    printf("%d", sum);
+    return sum;
+}
 
-	return 0;
+int main()
+{
+    int number;
+
+    printf("Enter number: ");
+    scanf("%d", &number);
+
+    printf("De quy: %d\n", DeQuy(number));
+    printf("Khu de quy: %d", KhuDeQuy(number));
+
+    return 0;
 }
